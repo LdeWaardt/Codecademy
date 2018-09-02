29a2140230b21337104f123c9099d25ee7d1dd54
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 10:09:46 -0500

    Complete 'Variables' exercise

---
+++ Python/1_Python_Syntax/5_Variables.py
@@ -0,0 +1,3 @@
+# In Python, and when programming in general, we need to build systems for dealing with data that changes over time. That data could be the location of a plane, or the time of day, or the television show you're currently watching. The only important thing is that it may be different at different times. Python uses variables to define things that are subject to change.
+# Create a variable called todays_date and assign a value that will represent today's date to that variable.
+todays_date = "July 30, 2018"
