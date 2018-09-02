4c1b2ba10490fe41226be6d155cdec55bde0ebb9
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 11:11:55 -0500

    Complete 'Strings' exercise

---
+++ Python/Strings & Console Output/01_Strings.py
@@ -0,0 +1,9 @@
+#Another useful data type is the string. A string can contain letters, numbers, and symbols.
+
+name = "Ryan"
+age = "19"
+food = "cheese"
+
+# PROBLEM: Create a new variable brian and assign it the string "Hello life!".
+
+brian = "Hello life!"
