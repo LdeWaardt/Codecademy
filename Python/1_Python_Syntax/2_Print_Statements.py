3f1a3fad5dc27acfdf9fd264108d28afb6a95fea
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 09:49:02 -0500

    Complete 'Print Statements' exercise

---
+++ Python/1_Python_Syntax/2_Print_Statements.py
@@ -0,0 +1,2 @@
+# There are two different Python versions. Both Python 2 and Python 3 are used throughout the globe. The most significant difference between the two is how you write a print statement. In Python 3, print has parentheses.
+print("I am doing it, Peter!")
