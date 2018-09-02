15f3d2cb8652182ec0f1c5b21cb113f920c6d0d3
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 10:30:59 -0500

    Complete 'Booleans' exercise

---
+++ Python/1_Python_Syntax/11_Booleans.py
@@ -0,0 +1,12 @@
+# Sometimes we have a need for variables that are either true or false. This datatype, which can only ever take one of two values, is called a boolean. In Python, we define booleans using the keywords True and False
+
+# A boolean is actually a special case of an integer. A value of True corresponds to an integer value of 1, and will behave the same. A value of False corresponds to an integer value of 0
+
+# PROBLEM: Someone has introduced themselves to you using comments in script.py. Read the comments and then create a variable called age_is_12 and set it to be True or False depending on if this person's age is 12.
+
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+
+age_is_12 = False
+name_is_maria = True
