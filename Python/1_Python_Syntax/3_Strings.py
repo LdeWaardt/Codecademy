0c1f06d3b68004ae9413df858d9da50f59416b8b
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 10:02:24 -0500

    Complete 'Strings' exercise

---
+++ Python/1_Python_Syntax/3_Strings.py
@@ -0,0 +1,2 @@
+ # Text in Python is considered a specific type of data called a string. A string, so named because they're a series of letters, numbers, or symbols connected in order — as if threaded together by string. Strings can be defined in different ways. You can use single quotes or double quotes. We can combine multiple strings using +
+ print "Hello " + "Lisa"
