4bb933b3feb48d6ce9700ea6510698d15309f71e
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 10:12:30 -0500

    Complete 'Arithmetic' exercise

---
+++ Python/1_Python_Syntax/6_Arithmetic.py
@@ -0,0 +1,14 @@
+mirthful_addition = 12381 + 91817
+amazing_subtraction = 981 - 312
+trippy_multiplication = 38 * 902
+happy_division = 540 / 45
+sassy_combinations = 129 * 1345 + 120 / 6 - 12
+
+# Above are a number of arithmetic operations, each assigned to a variable. The variable will hold the final result of each operation. Combinations of arithmetical operators follow the usual order of operations.
+
+# Python also offers a companion to division called the modulo operator. The modulo operator is indicated by % and returns the remainder after division is performed.
+
+product = 1 * 3
+remainder = 1398 % 11
+print product
+print remainder
