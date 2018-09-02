d42f2a0602aced8a7790420a79dc71352c110f68
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-02 10:21:25 -0500

    Complete 'Numbers' exercise

---
+++ Python/1_Python_Syntax/8_Numbers.py
@@ -0,0 +1,13 @@
+# Variables can also hold numeric values. The simplest kind of number in Python is the integer, which is a whole number with no decimal point:
+
+# A number with a decimal point is called a float. You can define floats with numbers after the decimal point or by just including a decimal point at the end:
+
+# You can also define a float using scientific notation, with e indicating the power of 10
+
+# PROBLEM: You are going shopping. Let's make a grocery list so that you can plan your budget. Store the number of cucumbers you want to buy in a variable called cucumbers. Make sure it's at least 1, and that it's the appropriate datatype! The store doesn't sell partial cucumbers. Each cucumber costs 3.25 doubloons. Store the price per cucumber in a variable called price_per_cucumber. Create a new variable called total_cost which is the product of how many cucumbers you are going to buy and the cost per cucumber. Print out total_cost. What datatype is it?
+
+cucumbers = 6
+# the price is in dubloons
+price_per_cucumber = 3.25 
+total_cost = cucumbers * price_per_cucumber
+print total_cost 
