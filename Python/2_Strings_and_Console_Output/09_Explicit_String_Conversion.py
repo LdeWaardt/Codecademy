b9fea8f16e3490d8a2ff402a3942bc64424c2089
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-03 09:46:16 -0500

    Complete 'Explicit String Conversion' exercise

---
+++ Python/2_Strings_and_Console_Output/09_Explicit_String_Conversion.py
@@ -0,0 +1,15 @@
+# Explicit String Conversion
+
+# Sometimes you need to combine a string with something that isn't a string. In order to do that, you have to convert the non-string into a string.
+
+print "I have " + str(2) + " coconuts!"
+
+# This will print I have 2 coconuts!.
+
+# The str() method converts non-strings into strings. In the above example, you convert the number 2 into a string and then you concatenate the strings together just like in the previous exercise.
+
+# Now try it yourself!
+
+# PROBLEM: Run the code as-is. You get an error! Use str() to turn 3.14 into a string. Then run the code again.
+
+print "The value of pi is around " + str(3.14)
