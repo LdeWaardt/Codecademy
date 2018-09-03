2021e891baa4d15aaa16acc3059189db592fcce0
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-03 09:25:08 -0500

    Complete 'String Method' exercise

---
+++ Python/2_Strings_and_Console_Output/04_String_Method.py
@@ -0,0 +1,14 @@
+# String methods let you perform specific tasks for strings.
+
+# We'll focus on four string methods:
+
+# len()
+# lower()
+# upper()
+# str()
+# Let's start with len(), which gets the length (the number of characters) of a string!
+
+# PROBLEM: create a variable named parrot and set it to the string "Norwegian Blue". Type len(parrot) after the word print, like so: print len(parrot). The output will be the number of characters in "Norwegian Blue"!
+
+parrot = "Norwegian Blue"
+print len(parrot)
