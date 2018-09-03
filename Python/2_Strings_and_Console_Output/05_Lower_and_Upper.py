76ef2cd26fd0ba97fbe77f1aa30346c74d4d303b
Lisa de Waardt <Lisadewaardt@gmail.com>
2018-09-03 09:32:26 -0500

    Complete 'Lower case and Upper Case' exercise

---
+++ Python/2_Strings_and_Console_Output/05_Lower_and_Upper.py
@@ -0,0 +1,15 @@
+#You can use the lower() method to get rid of all the capitalization in your strings. You call lower() like so:
+
+"Ryan".lower()
+# which will return "ryan".
+
+# PROBLEM: Call lower() on parrot (after print) on line 3 in the editor
+
+parrot = "Norwegian Blue"
+print len(parrot)
+print parrot.lower()
+
+# PROBLEM: Call upper() on parrot (after print on line 3) in order to capitalize all the characters in the string!
+
+parrot = "norwegian blue"
+print parrot.upper()
